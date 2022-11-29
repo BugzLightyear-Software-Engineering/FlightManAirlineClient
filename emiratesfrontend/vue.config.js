e82634e8c21cb52070397348f71858bf2b89de36
otito12 <titodarluzu@gmail.com>
2022-11-29 09:25:03 -0500

    s

--- emiratesfrontend/vue.config.js
+++ emiratesfrontend/vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   devServer: {
+    allowedHosts: "all",
     proxy: "https://bugzlightyear-flightman.herokuapp.com/",
   },
   transpileDependencies: ["vuetify"],
